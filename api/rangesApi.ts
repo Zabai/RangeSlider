0c7e85732985c0192bc38ae31c3129bbb8ed7b7a
Zabai Armas Herrera <zbrmas@hotmail.com>
2023-06-14 13:28:08 +0100

    feat: add schema validations to api calls

--- api/rangesApi.ts
+++ api/rangesApi.ts
@@ -2,18 +2,19 @@ import {
   GET_FIXED_RANGES_ENDPOINT,
   GET_NORMAL_RANGES_ENDPOINT,
 } from "../constants/endpoints";
+import { fixedRangesApiSchema, normalRangesApiSchema } from "../schemas/ranges";
 import { FixedRange, NormalRange } from "../types/ranges";
 import { fetchJson } from "../utils/fetch";
 import { getRandomItemFromArray } from "../utils/random";
 
 export function getFixedRangeApi() {
-  return fetchJson<FixedRange[]>(GET_FIXED_RANGES_ENDPOINT).then((ranges) =>
-    getRandomItemFromArray(ranges)
-  );
+  return fetchJson<FixedRange[]>(GET_FIXED_RANGES_ENDPOINT)
+    .then((ranges) => fixedRangesApiSchema.validate(ranges))
+    .then((ranges) => getRandomItemFromArray(ranges));
 }
 
 export function getNormalRangeApi() {
-  return fetchJson<NormalRange[]>(GET_NORMAL_RANGES_ENDPOINT).then((ranges) =>
-    getRandomItemFromArray(ranges)
-  );
+  return fetchJson<NormalRange[]>(GET_NORMAL_RANGES_ENDPOINT)
+    .then((ranges) => normalRangesApiSchema.validate(ranges))
+    .then((ranges) => getRandomItemFromArray(ranges));
 }
