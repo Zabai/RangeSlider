b24c989b14037c0b6c983dbdb26259aaa7cb47a8
Zabai Armas Herrera <zbrmas@hotmail.com>
2023-06-14 11:48:10 +0100

    feat: add endpoints

---
+++ constants/endpoints.ts
@@ -0,0 +1,4 @@
+export const GET_FIXED_RANGES_ENDPOINT =
+  "https://run.mocky.io/v3/695e3ba7-e1aa-4ab6-8c40-62f5dce7460c";
+export const GET_NORMAL_RANGES_ENDPOINT =
+  "https://run.mocky.io/v3/7ca1cfe3-d452-4c0d-878d-0c14c348c38a";
