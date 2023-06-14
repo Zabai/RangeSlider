77842c41584ef66c92c3d62cceaf750d8cdf7f6d
Zabai Armas Herrera <zbrmas@hotmail.com>
2023-06-14 13:14:40 +0100

    fix: required fields in range schemas

--- schemas/ranges.ts
+++ schemas/ranges.ts
@@ -1,13 +1,25 @@
 import * as yup from "yup";
 import { NormalRange } from "../types/ranges";
 
-const fixedRangeSchema = yup.array().of(yup.number()).min(3);
+const fixedRangeSchema = yup
+  .array()
+  .of(yup.number().required())
+  .required()
+  .min(3);
 
-export const fixedRangesApiSchema = yup.array().of(fixedRangeSchema).min(1);
+export const fixedRangesApiSchema = yup
+  .array()
+  .of(fixedRangeSchema)
+  .required()
+  .min(1);
 
 const normalRangeSchema: yup.ObjectSchema<NormalRange> = yup.object({
   max: yup.number().required(),
   min: yup.number().required(),
 });
 
-export const normalRangesApiSchema = yup.array().of(normalRangeSchema).min(1);
+export const normalRangesApiSchema = yup
+  .array()
+  .of(normalRangeSchema)
+  .required()
+  .min(1);
