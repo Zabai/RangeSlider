05b46106b43de0cd22aea2ae0772de9d7d4e12cc
Zabai Armas Herrera <zbrmas@hotmail.com>
2023-06-18 20:18:31 +0100

    feat: index page links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+
 export default function Index() {
-  return <h1>Index page</h1>;
+  return (
+    <>
+      <h1>Home page</h1>
+
+      <ul>
+        <li>
+          <Link href="/exercise1">Exercise 1</Link>
+        </li>
+        <li>
+          <Link href="/exercise2">Exercise 2</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
